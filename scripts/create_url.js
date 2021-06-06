9cbae6972a8b471fb95a53a597a708f9148b28fe
Zander <60998830+CredibleOpossum@users.noreply.github.com>
2021-06-06 09:22:17 -0400

    Initial commit

---
+++ scripts/create_url.js
@@ -0,0 +1,31 @@
+function submit() {
+    let url = document.getElementById("url");
+    let key = document.getElementById("key");
+    if (url.value == "") {
+        invaild(url);
+        return;
+    }
+    if (key.value == "") {
+        invaild(key);
+        return;
+    }
+    url = `https://${
+        window.location.hostname
+    }/index.html?url=${CryptoJS.AES.encrypt(url.value, key.value)}`;
+    document.getElementById(
+        "urlField"
+    ).innerHTML += `<br></br><a href=\"${url}\">${url}</a>`;
+}
+
+function invaild(element) {
+    element.style.backgroundColor = "red";
+    setTimeout(function () {
+        element.style.backgroundColor = "white";
+    }, 500);
+}
+
+function keyPress(event) {
+    if (event.keyCode == 13) {
+        submit();
+    }
+}
---
+++ scripts/encrypted_url.js
@@ -0,0 +1,43 @@
+"use strict";
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(
+        /[?&]+([^=&]+)=([^&]*)/gi,
+        function (m, key, value) {
+            vars[key] = value;
+        }
+    );
+    return vars;
+}
+
+if (!getUrlVars().url) {
+    window.location = "create_url.html";
+}
+
+function invaild(element) {
+    element.style.backgroundColor = "red";
+    setTimeout(function () {
+        element.style.backgroundColor = "white";
+    }, 500);
+}
+
+function keyPress(event) {
+    if (event.keyCode == 13) {
+        let passwordInput = document.getElementById("key");
+        try {
+            let decryptedUrl = CryptoJS.AES.decrypt(
+                getUrlVars().url,
+                passwordInput.value
+            ).toString(CryptoJS.enc.Utf8);
+            if (decryptedUrl) {
+                window.location.href = decryptedUrl;
+            } else {
+                invaild(passwordInput);
+            }
+        } catch (err) {
+            console.log(err);
+            invaild(passwordInput);
+        }
+    }
+}
